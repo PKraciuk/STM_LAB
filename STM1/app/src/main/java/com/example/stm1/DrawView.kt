6d486a28544b4820990649e3681a9e8110ab506a
pkraciuk <pawelkraciuk@gmail.com>
2023-12-15 23:57:02 +0100

    add multi

--- STM1/app/src/main/java/com/example/stm1/DrawView.kt
+++ STM1/app/src/main/java/com/example/stm1/DrawView.kt
@@ -2,12 +2,33 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.View
+import com.google.gson.Gson
+
+
+data class GameData(
+    val paddle1X: Float,
+    val paddle1Y: Float,
+    val ballX: Float,
+    val ballY: Float,
+    val scorePlayer1: Int,
+    val scorePlayer2: Int
+
+)
+{
+    fun toJson(): String = Gson().toJson(this)
+
+    companion object {
+        fun fromJson(json: String): GameData = Gson().fromJson(json, GameData::class.java)
+    }
+}
+
 
 class DrawView(context: Context) : View(context) {
-    private var width = 0.0f
-    private var height = 0.0f
+    private var width = 1.0f
+    private var height = 1.0f
     private var paint: Paint = Paint()
     private var ballX = 200f
     private var ballY = 200f
@@ -33,20 +54,35 @@ class DrawView(context: Context) : View(context) {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
+        // Constants for design dimensions (based on your original design)
+        val DESIGN_WIDTH = 1440f  // Replace with your design's width
+        val DESIGN_HEIGHT = 3120f // Replace with your design's height
 
+        // Calculate scale factors for width and height
+        val scaleFactorX = w / DESIGN_WIDTH
+        val scaleFactorY = h / DESIGN_HEIGHT
+
+        // Update screen dimensions
         width = w.toFloat()
         height = h.toFloat()
 
-        // Inicjalizacja pozycji paletki i piłki
-        paddle1Y = height - 300
-        paddle2Y = 300.0f
-        paddle1X = width / 2 - paddleWidth / 2
-        paddle2X = width / 2 - paddleWidth / 2
+        // Scale the dimensions of the paddles and ball
+        paddleWidth *= scaleFactorX
+        paddleHeight *= scaleFactorY
         ballX = width / 2
         ballY = height / 2
 
+        // Adjust the positions of the paddles
+        paddle1Y = height - 300 * scaleFactorY
+        paddle2Y = 300 * scaleFactorY
+        paddle1X = width / 2 - paddleWidth / 2
+        paddle2X = width / 2 - paddleWidth / 2
+
+        // Reset scores
         scorePlayer1 = 0
         scorePlayer2 = 0
+
+        // If you have other elements that need scaling, adjust them here
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -76,19 +112,29 @@ class DrawView(context: Context) : View(context) {
 
 
         if (ballX + 20 >= paddle2X && ballX - 20 <= paddle2X + paddleWidth && ballY + 20 >= paddle2Y && ballY - 20 <= paddle2Y + paddleHeight) {
-            if(ballSpeedY < 0) {
+            if (ballSpeedY < 0) {
                 ballSpeedY = -ballSpeedY // Odbicie piłki
                 ballAcceleration = -ballAcceleration
                 ballSpeedY *= 0.8f
+
+                // Adjust ballSpeedX based on paddle hit location
+                val paddleCenter = paddle2X + paddleWidth / 2
+                val hitPosition = ballX - paddleCenter
+                ballSpeedX += hitPosition / paddleWidth * 2 // Adjust the multiplier as needed
             }
         }
 
 
         if (ballX - 20 <= paddle1X + paddleWidth && ballX + 20 >= paddle1X && ballY - 20 <= paddle1Y + paddleHeight && ballY + 20 >= paddle1Y) {
-            if(ballSpeedY>0) {
+            if (ballSpeedY > 0) {
                 ballSpeedY = -ballSpeedY // Odbicie piłki
                 ballAcceleration = -ballAcceleration
                 ballSpeedY *= 0.8f
+
+                // Adjust ballSpeedX based on paddle hit location
+                val paddleCenter = paddle1X + paddleWidth / 2
+                val hitPosition = ballX - paddleCenter
+                ballSpeedX += hitPosition / paddleWidth * 2 // Adjust the multiplier as needed
             }
         }
 
@@ -192,13 +238,43 @@ class DrawView(context: Context) : View(context) {
             // Przesuń górną paletkę
             paddle1X = x - paddleWidth / 2
             paddle1X = Math.max(0f, Math.min(paddle1X, width - paddleWidth))
-        } else {
-            // Przesuń dolną paletkę
-            paddle2X = x - paddleWidth / 2
-            paddle2X = Math.max(0f, Math.min(paddle2X, width - paddleWidth))
         }
     }
 
+    fun prepareGameData(): GameData {
+        // Capture the current state of the game
+        return GameData(
+            paddle1X = paddle1X / width,   // Store as relative position
+            paddle1Y = paddle1Y / height,  // Store as relative position
+            ballX = ballX / width,         // Store as relative position
+            ballY = ballY / height,        // Store as relative position
+            scorePlayer1 = scorePlayer1,
+            scorePlayer2 = scorePlayer2
+        )
+    }
+
+    fun updateGameState(gameData: GameData) {
+        // Convert relative positions back to pixel values based on local screen size
+        this.ballX = width - (gameData.ballX * width) // gameData.ballX is a relative value (0 to 1)
+        this.ballY = height - (gameData.ballY * height)
+        this.paddle2X = width - (gameData.paddle1X * width) - paddleWidth
+        this.paddle2Y = height - (gameData.paddle1Y * height)
+        this.scorePlayer1 = gameData.scorePlayer2
+        this.scorePlayer2 = gameData.scorePlayer1
+        invalidate() // Redraw the view with updated data
+    }
+
+    fun setEnemyPaddle(relativeX: Float) {
+        // Convert the relative position to a pixel value
+        this.paddle2X = relativeX * width
+        invalidate() // Redraw the view with updated data
+    }
+
+
+    fun getPaddleX(): Float {
+        return paddle1X/width
+
+    }
 
 
 }
--- STM1/app/src/main/java/com/example/stm1/MainActivity.kt
+++ STM1/app/src/main/java/com/example/stm1/MainActivity.kt
@@ -1,27 +1,153 @@
 package com.example.stm1
 
 import DrawView
+import GameData
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.AsyncTask
 import android.os.Bundle
+import android.text.format.Formatter
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.stm1.ui.theme.STM1Theme
+import java.io.*
+import java.net.ServerSocket
+import java.net.Socket
+
 
 class MainActivity : ComponentActivity() {
+
+    private var serverSocket: ServerSocket? = null
+    private val serverPort = 8080
+    private var serverThread: SocketServerThread? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start_screen)
 
-        val layout = findViewById<ConstraintLayout>(R.id.main_layout) // Zmień na ID twojego głównego layoutu
+        setupStartScreen()
+    }
+
+    private fun setupStartScreen() {
+        val hostButton = findViewById<Button>(R.id.btnHostGame)
+        val joinButton = findViewById<Button>(R.id.btnJoinGame)
+        val ipEditText = findViewById<EditText>(R.id.etEnterIp)
+        val ipAddressTextView = findViewById<TextView>(R.id.tvDisplayIp) // Assuming you have a TextView for displaying IP
         val drawView = DrawView(this)
-        layout.addView(drawView)
+
+        hostButton.setOnClickListener {
+            val ip = getLocalIpAddress()
+            ipAddressTextView.text = "IP Address: $ip" // Display IP Address on the screen
+            Log.d("MainActivity", "Host button clicked, IP: $ip")
+            serverThread = SocketServerThread(this, drawView)
+            serverThread!!.start()
+        }
+
+        joinButton.setOnClickListener {
+            val ip = ipEditText.text.toString()
+            Client(ip, this, drawView).execute()
+        }
+    }
+
+    private inner class SocketServerThread(private val context: Context, private val drawView: DrawView) : Thread() {
+        override fun run() {
+            try {
+                serverSocket = ServerSocket(serverPort)
+                while (!isInterrupted) {
+                    val socket = serverSocket!!.accept()
+                    val input = BufferedReader(InputStreamReader(socket.getInputStream()))
+                    val output = PrintStream(socket.getOutputStream())
+
+                    runOnUiThread {
+                        val hostButton = findViewById<Button>(R.id.btnHostGame)
+                        val joinButton = findViewById<Button>(R.id.btnJoinGame)
+                        val ipEditText = findViewById<EditText>(R.id.etEnterIp)
+                        val ipAddressTextView = findViewById<TextView>(R.id.tvDisplayIp)
+                        hostButton.setVisibility(View.GONE);
+                        joinButton.setVisibility(View.GONE);
+                        ipEditText.setVisibility(View.GONE);
+                        ipAddressTextView.setVisibility(View.GONE);
+                        val layout = findViewById<RelativeLayout>(R.id.main_layout)
+                        layout.addView(drawView)
+
+                    }
+
+                    while (!socket.isClosed) {
+                        val enemyPaddle = input.readLine()
+                        drawView.setEnemyPaddle(enemyPaddle.toFloat())
+
+                        val serverGameData = drawView.prepareGameData()
+                        val serverGameDataJson = serverGameData.toJson()
+                        output.println(serverGameDataJson)
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private inner class Client(private val dstAddress: String, private val context: Context, private val drawView: DrawView) : AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg params: Void): String? {
+            try {
+                val socket = Socket(dstAddress, serverPort)
+                val output = PrintStream(socket.getOutputStream())
+                val input = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                runOnUiThread {
+                    val hostButton = findViewById<Button>(R.id.btnHostGame)
+                    val joinButton = findViewById<Button>(R.id.btnJoinGame)
+                    val ipEditText = findViewById<EditText>(R.id.etEnterIp)
+                    val ipAddressTextView = findViewById<TextView>(R.id.tvDisplayIp)
+                    hostButton.setVisibility(View.GONE);
+                    joinButton.setVisibility(View.GONE);
+                    ipEditText.setVisibility(View.GONE);
+                    ipAddressTextView.setVisibility(View.GONE);
+                    val layout = findViewById<RelativeLayout>(R.id.main_layout)
+                    layout.addView(drawView)
+                }
+
+                while (!socket.isClosed) {
+                    // Send only paddle position
+                    val paddleX: Float =
+                        drawView.getPaddleX() // Assuming you have a method to get paddle X position
+                    output.println(paddleX)
+                    Log.wtf("I SENT",paddleX.toString())
+
+                    // Receive and update game state
+                    val serverGameDataJson = input.readLine()
+                    Log.wtf("RECEIVED FROM SERVER", serverGameDataJson.toString())
+                    runOnUiThread {
+                        drawView.updateGameState(GameData.fromJson(serverGameDataJson))
+                    }
+                }
+
+                socket.close()
+                return null
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return null
+            }
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+    }
+
+    private fun getLocalIpAddress(): String {
+        val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        val ipAddress = wifiManager.connectionInfo.ipAddress
+        return Formatter.formatIpAddress(ipAddress)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serverThread?.interrupt()
+        serverSocket?.close()
     }
 }
-
